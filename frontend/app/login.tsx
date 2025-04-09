3840ac637d74489471d6842d8b40c712a1b7801f
Avokadodib <avocado.dib@gmail.com>
2025-04-09 15:29:27 +0700

    form register&&login

--- frontend/app/login.tsx
+++ frontend/app/login.tsx
@@ -1,6 +1,6 @@
-import { StyleSheet, Text,TextInput, View, Image, TouchableOpacity, StatusBar } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Image, TouchableOpacity, StatusBar, KeyboardAvoidingView, Platform } from 'react-native';
 import { useState } from 'react';
-import {images} from "@/constants/images"
+import { images } from "@/constants/images"
 import { icons } from '@/constants/icon'
 import { Link } from 'expo-router';
 export default function RegisterScreen() {
@@ -12,7 +12,7 @@ export default function RegisterScreen() {
       {/* Logo + tên app */}
       <View style={styles.logoContainer}>
         <View>
-        <Image source={images.logo} style={styles.logo}/>
+          <Image source={images.logo} style={styles.logo} />
         </View>
         <Text style={styles.appTitle}>Healthy tracker</Text>
       </View>
@@ -40,7 +40,7 @@ export default function RegisterScreen() {
       <Text style={styles.orText}>or</Text>
       <TouchableOpacity style={styles.googleButton}>
         <Text style={styles.googleIcon}>
-         <Image source={icons.google} style={styles.googleIcon}/>
+          <Image source={icons.google} style={styles.googleIcon} />
         </Text>
         <Text style={styles.googleText}>Continue with Google</Text>
       </TouchableOpacity>
@@ -55,99 +55,98 @@ export default function RegisterScreen() {
   );
 }
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#F3F4F6',
-      paddingHorizontal: 24,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    logoContainer: {
-      alignItems: 'center',
-      marginBottom: 24,
-    },
-    logo: {
-        width: 120,
-        height: 120,
-        resizeMode: 'contain',
-        marginBottom: 90,
-    },
-    appTitle: {
-      fontSize: 20,
-      fontWeight: 'bold',
-      bottom:85,
-    },
-    header: {
-      fontSize: 18,
-      fontWeight: '600',
-      marginBottom: 4,
-    },
-    subHeader: {
-      color: '#6B7280',
-      marginBottom: 16,
-      textAlign: 'center',
-    },
-    input: {
-      backgroundColor: 'white',
-      paddingHorizontal: 16,
-      paddingVertical: 12,
-      borderRadius: 8,
-      width: '100%',
-      marginBottom: 12,
-    },
-    continueButton: {
-      backgroundColor: 'black',
-      paddingVertical: 14,
-      borderRadius: 8,
-      width: '100%',
-      marginTop: 8,
-    },
-    continueText: {
-      color: 'white',
-      textAlign: 'center',
-      fontWeight: '600',
-    },
-    loginText: {
-      color: '#4B5563',
-      marginVertical: 12,
-      fontSize: 14,
-    },
-    loginLink: {
-      color: 'black',
-      fontWeight: '600',
-    },
-    orText: {
-      color: '#9CA3AF',
-      marginBottom: 12,
-      fontSize: 14,
-    },
-    googleButton: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: '#F3F4F6',
-      borderColor: '#D1D5DB',
-      borderWidth: 1,
-      paddingVertical: 12,
-      borderRadius: 8,
-      width: '100%',
-      marginBottom: 20,
-    },
-    googleIcon: {
-      width:15,
-      height:15,
-      marginRight: 10,
-    },
-    googleText: {
-      fontWeight: '500',
-    },
-    policyText: {
-      fontSize: 12,
-      textAlign: 'center',
-      color: '#000000',
-    },
-    linkText: {
-      fontWeight:"600",
-    },
-  });
-  
+  container: {
+    flex: 1,
+    backgroundColor: '#F3F4F6',
+    paddingHorizontal: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logoContainer: {
+    alignItems: 'center',
+    marginBottom: 24,
+  },
+  logo: {
+    width: 120,
+    height: 120,
+    resizeMode: 'contain',
+    marginBottom: 90,
+  },
+  appTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    bottom: 85,
+  },
+  header: {
+    fontSize: 18,
+    fontWeight: '600',
+    marginBottom: 4,
+  },
+  subHeader: {
+    color: '#6B7280',
+    marginBottom: 16,
+    textAlign: 'center',
+  },
+  input: {
+    backgroundColor: 'white',
+    paddingHorizontal: 16,
+    paddingVertical: 12,
+    borderRadius: 8,
+    width: '100%',
+    marginBottom: 12,
+  },
+  continueButton: {
+    backgroundColor: 'black',
+    paddingVertical: 14,
+    borderRadius: 8,
+    width: '100%',
+    marginTop: 8,
+  },
+  continueText: {
+    color: 'white',
+    textAlign: 'center',
+    fontWeight: '600',
+  },
+  loginText: {
+    color: '#4B5563',
+    marginVertical: 12,
+    fontSize: 14,
+  },
+  loginLink: {
+    color: 'black',
+    fontWeight: '600',
+  },
+  orText: {
+    color: '#9CA3AF',
+    marginBottom: 12,
+    fontSize: 14,
+  },
+  googleButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#F3F4F6',
+    borderColor: '#D1D5DB',
+    borderWidth: 1,
+    paddingVertical: 12,
+    borderRadius: 8,
+    width: '100%',
+    marginBottom: 20,
+  },
+  googleIcon: {
+    width: 15,
+    height: 15,
+    marginRight: 10,
+  },
+  googleText: {
+    fontWeight: '500',
+  },
+  policyText: {
+    fontSize: 12,
+    textAlign: 'center',
+    color: '#000000',
+  },
+  linkText: {
+    fontWeight: "600",
+  },
+});

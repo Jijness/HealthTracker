7799fc309e9c985f2b2ee27c46352e8ad709191d
Avokadodib <avocado.dib@gmail.com>
2025-04-09 15:39:17 +0700

    Lấy frontend từ nhánh tronghm

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,5 +1,34 @@
 import { Stack } from "expo-router";
-
+import './global.css';
 export default function RootLayout() {
-  return <Stack />;
+  return(
+  <Stack>
+    <Stack.Screen
+      name="(tabs)"
+      options={{
+        headerShown:false, 
+      }}
+    />
+    <Stack.Screen
+      name="index"
+      options={{
+        title:"Intro",
+        headerShown:false,
+      }}
+    />
+    <Stack.Screen
+      name="register"
+      options={{
+        title:"Register",
+        headerShown:false,
+      }}
+    />
+    <Stack.Screen
+      name="login"
+      options={{
+        title:"Login",
+        headerShown:false,
+      }}
+    />
+  </Stack>)
 }
---
+++ frontend/constants/icon.ts
@@ -0,0 +1,4 @@
+import google from "@/assets/icons/google.png"
+export const icons ={
+    google,
+}
